d071ebe3c1dc23a1b9044d574eb77d4b4cc6a5af
Muhammad Ali <133563431+MuhAliejaz47@users.noreply.github.com>
2023-08-26 12:14:41 +0500

    Add files via upload

---
+++ NLP Text Sentiment Analysis/NLP Text Sentiment Analysis.py
@@ -0,0 +1,18 @@
+from textblob import TextBlob
+from newspaper import Article
+
+url = 'https://en.wikipedia.org/wiki/Mathematics'
+
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.summary
+print(text)
+
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity # -1 to 1
+print(sentiment)
+
